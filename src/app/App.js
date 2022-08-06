885bfb5d6bf078686542462d3bd1afa5408430bf
rashidya <rashmiliyanage2000@gmail.com>
2022-08-07 01:55:07 +0530

    DashBoard UI Done

--- src/app/App.js
+++ src/app/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import LogInIndex from "../pages/LogIn/logInIndex";
 import {Route,Routes} from "react-router-dom";
 import UserRegistration from "../pages/UserRegistration";
+import DashBoard from "../pages/DashBoard/dashBoard";
 
 
 function App() {
@@ -9,7 +10,7 @@ function App() {
      /* <Routes>
           <Route exact path='/' element={ <LogInIndex/>}/>
       </Routes>*/
-      <UserRegistration/>
+      <DashBoard/>
   );
 }
 
